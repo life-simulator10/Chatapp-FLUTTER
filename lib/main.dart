842141aabd36237a9beff5ea21fce39645e63401
Bishwa Pangeni <bishwapangeni39@gmail.com>
2023-07-28 08:10:28 +0545

    structuring of the folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
+import 'package:chatapp/pages/home_page.dart';
+import 'package:chatapp/shared/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    // run the initialization for web
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: Constants.apiKey,
+          appId: Constants.appId,
+          messagingSenderId: Constants.messagingSenderId,
+          projectId: Constants.projectId),
+    );
+  } else {
+    //run the initialization for android
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
@@ -12,6 +28,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
