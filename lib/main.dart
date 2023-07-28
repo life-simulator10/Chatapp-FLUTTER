d4b55b7778417cbec992daee5055469c4aaa2c7e
Bishwa Pangeni <bishwapangeni39@gmail.com>
2023-07-28 08:41:38 +0545

    Login Page creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:chatapp/helper/helper_function.dart';
+import 'package:chatapp/pages/auth/login_page.dart';
 import 'package:chatapp/pages/home_page.dart';
 import 'package:chatapp/shared/constants.dart';
 import 'package:flutter/material.dart';
@@ -23,14 +25,35 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isSignedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserLoggedInStatus();
+  }
+
+  getUserLoggedInStatus() async {
+    await HelperFunctions.getUserLoggedInStatus().then((value) {
+      if (value != null) {
+        _isSignedIn = value;
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: _isSignedIn ? const HomePage() : const LoginPage(),
     );
   }
 }
